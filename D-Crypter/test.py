66a480bf64646026706bfd1c08bfe36762441ee5
BlaZeSama <adityasinghajeet@gmail.com>
2021-03-28 04:57:15 +0530

    OwO

--- D-Crypter/test.py
+++ D-Crypter/test.py
@@ -1,17 +1,11 @@
-PolybiusList = [['A','B','C','D','E'],['F','G','H','I,J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']]
-C = '23331223112432112422113314452345'
-Li = []
-for i in C:
-    i = int(i)
-    Li.append(i)
-print(Li)
-Result = ''
-for i in range(0,len(C),2):
-    print(i)
-    R,C = Li[i],Li[i+1]
-    Result += PolybiusList[R-1][C-1]
-
-L1 = [1,2]
-L2 = [2,3]
-NL = [L1,L2]
-print(NL)
+Input = [12,34,56,78,91]
+Key = [12,34,56]
+Keyword = []
+while len(Keyword)<len(Input):
+    if len(Input)-len(Keyword)<len(Key):
+        n = len(Input)-len(Keyword)
+        Keyword += Key[0:n]
+    else:
+        n = len(Input)-len(Key)
+        Keyword += Key[0:n+2]
+print(Keyword)
